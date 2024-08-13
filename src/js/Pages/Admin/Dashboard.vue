e9fb79d492ce3edebdbf989454dfd58135edbec4
favourch <fchukwuedo@gmail.com>
2024-08-13 08:43:12 -0700

    welcome Gideon

--- src/js/Pages/Admin/Dashboard.vue
+++ src/js/Pages/Admin/Dashboard.vue
@@ -12,7 +12,7 @@
               {{ `Welcome back, ${user.first_name}` }} 👋
             </span>
             <span v-else class="mt-1 font-semibold md:font-normal text-xl">
-              Welcome back 👋
+               Gideon👋
             </span>
           </p>
         </div>
